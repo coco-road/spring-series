6218aa83bcab765f19af7157e7d109760dc9923f
coco-road <fenglei_apple@163.com>
2021-08-17 22:45:53 +0800

    git third commit

--- lesson-001/src/main/java/com/coco/lesson002/demo1/GitTest.java
+++ lesson-001/src/main/java/com/coco/lesson002/demo1/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git version");
         System.out.println("hello git version 2");
+        System.out.println("hello git version 3");
     }
 }
