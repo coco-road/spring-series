e874c3259beb532f331c7a1964e4ac1e1abdc795
coco-road <fenglei_apple@163.com>
2021-08-18 17:06:31 +0800

    git remote push commit

--- lesson-001/src/main/java/com/coco/lesson002/demo1/GitTest.java
+++ lesson-001/src/main/java/com/coco/lesson002/demo1/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git hot-fix");
         System.out.println("hello master");
         System.out.println("hello git hot-fix 2");
+        System.out.println("hello push");
     }
 }
